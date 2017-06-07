accd781aea49c5cadcdd05adac9a7ce4db528bc3
Greg Arndt <arndt.greg@gmail.com>
2017-06-07 16:50:14 -0500

    Add tasks resource to URI

--- src/api.js
+++ src/api.js
@@ -80,3 +80,41 @@ api.declare({
   });
   return res.reply(result);
 });
+
+/** Worker Information **/
+api.declare({
+  name: 'worker',
+  method: 'get',
+  route: '/worker-groups/:workerGroup/workers/:workerId/tasks',
+  output: 'describe-worker-response.json#',
+  stability: API.stability.experimental,
+  title: 'Describe Worker',
+  description: 'List details known about a given worker ID',
+}, async function(req, res) {
+  let workerGroup = req.params.workerGroup;
+  let workerId = req.params.workerId;
+  // We should only need to return the last 100 tasks to know meaningful information
+  // about a worker.  Perhaps later we can make this a query param with offset.
+  let taskLimit = 100;
+
+  let data = await this.db.query(
+    'SELECT * from tasks where worker_id = $1 order by started desc limit $2',
+    [workerId, taskLimit]
+  );
+
+  let result = {
+    workerGroup,
+    workerId,
+    tasks: [],
+  };
+  data.rows.forEach(r => {
+    let newTask = {};
+    Object.keys(r).forEach(k => {
+      let v = r[k];
+      let newKey = k.replace(/_([a-z])/g, (g) => {return g[1].toUpperCase();});
+      newTask[newKey] = v;
+    });
+    result.tasks.push(newTask);
+  });
+  return res.reply(result);
+});
