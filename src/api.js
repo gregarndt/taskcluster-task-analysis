61caa9a4a0bb3536ded02a548f9fc393bf0d4e0f
Greg Arndt <arndt.greg@gmail.com>
2017-06-07 12:56:15 -0500

    Make resources in URLs more REST like

--- src/api.js
+++ src/api.js
@@ -17,7 +17,7 @@ module.exports = api;
 api.declare({
   name:   'listWorkerGroup',
   method: 'get',
-  route:  '/workers/:workerGroup',
+  route:  '/workerGroups/:workerGroup/workers',
   query: {
     limit: /^[0-9]+$/,
   },
@@ -47,7 +47,7 @@ api.declare({
 api.declare({
   name: 'worker',
   method: 'get',
-  route: '/workers/:workerGroup/:workerId',
+  route: '/workerGroups/:workerGroup/workers/:workerId',
   output: 'describe-worker-response.json#',
   stability: API.stability.experimental,
   title: 'Describe Worker',
